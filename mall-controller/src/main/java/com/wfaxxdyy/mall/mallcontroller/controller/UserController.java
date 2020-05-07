f62505669554dd3fc6652973e5c1a9de8ce6b382
wfaxxdyy <wfaxxdyy@163.com>
2020-05-07 16:38:28 +0800

    项目搭建6，2020.5.7
    重点解决跨域session的问题

--- mall-controller/src/main/java/com/wfaxxdyy/mall/mallcontroller/controller/UserController.java
+++ mall-controller/src/main/java/com/wfaxxdyy/mall/mallcontroller/controller/UserController.java
@@ -7,6 +7,7 @@ import com.wfaxxdyy.mallinterface.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -19,17 +20,23 @@ public class UserController {
     /*
     * 校验：根据username查询用户信息
     * */
-    @CrossOrigin(origins = "*", maxAge = 3600)
+    @CrossOrigin(origins = "*", maxAge = 3600,allowCredentials="true")
     @RequestMapping("/login")
     @ResponseBody
-    public User find(@RequestParam("username") String username){
+    public User find(@RequestParam("username") String username, HttpSession session){
         User user = userService.getUser(username);
         if(user==null){
             return null;
         }
+
+        session.setAttribute("user",user);
+
         return user;
     }
 
+    /*
+    * 注册
+    * */
     @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping("/register")
     @ResponseBody
@@ -40,5 +47,28 @@ public class UserController {
     }
 
 
+    /*
+    * 获取session用户对象
+    * */
+    @CrossOrigin(origins = "*", allowCredentials="true")
+    @RequestMapping("/getUserInfo")
+    @ResponseBody
+    public User getUserInfo(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if(user==null){
+            return null;
+        }
+        return user;
+    }
+
+    /*
+    * 注销session用户
+    * */
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    @RequestMapping("/delUserInfo")
+    @ResponseBody
+    public void delUserInfo(HttpSession session){
+        session.invalidate();
+    }
 
 }
